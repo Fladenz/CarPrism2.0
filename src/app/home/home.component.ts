da88b364710fec1a0b69a589ff86918924bebc35
nicolas <nicolas@local>
2026-03-01 16:55:34 -0300

    Fix(home): manage car images with Angular bindings and handlers

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,7 +11,34 @@ import { Router } from '@angular/router';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  carImages: string[] = [
+    'https://images.unsplash.com/photo-1503376763036-066120622c74?q=80&w=2070&auto=format&fit=crop',
+    'https://images.unsplash.com/photo-1617788138017-80ad40651399?q=80&w=2070&auto=format&fit=crop',
+    'https://images.unsplash.com/photo-1614162692292-7ac56d7f7f1e?q=80&w=2070&auto=format&fit=crop'
+  ];
+
+  currentIndex = 0;
+  imageLoaded = false;
+
   constructor(private router: Router) {}
+
+  get currentImage() {
+    return this.carImages[this.currentIndex];
+  }
+
+  prevImage() {
+    this.currentIndex = (this.currentIndex === 0) ? this.carImages.length - 1 : this.currentIndex - 1;
+    this.imageLoaded = false;
+  }
+
+  nextImage() {
+    this.currentIndex = (this.currentIndex === this.carImages.length - 1) ? 0 : this.currentIndex + 1;
+    this.imageLoaded = false;
+  }
+
+  onImageLoad() {
+    this.imageLoaded = true;
+  }
 
   goLogin() {
     this.router.navigate([{ outlets: { login: ['modal-login'] } }]);
